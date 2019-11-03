d24e5f5f5f09b3d6129572fc2e37c5262c09ef28
Marko Cupic <m.cupic@gmx.ch>
2019-11-03 20:38:02 +0100

    dev

--- assets/js/sidebar_navigation.js
+++ assets/js/sidebar_navigation.js
@@ -1,17 +1,18 @@
 jQuery(document).ready(function () {
     var elToggler = jQuery('<i class="toggle-submenu" role="button"> </i>');
-    window.setTimeout(function() {
-        jQuery(elToggler).insertAfter(".sidebar-navigation li.submenu > a, .sidebar-navigation li.submenu > strong");
-        jQuery('.sidebar-navigation li.submenu.trail, .sidebar-navigation li.active').addClass('expanded');
-    },2000);
-    jQuery(elToggler).click(function (e) {
-        e.preventDefault();
-        e.stopPropagation();
+    jQuery(elToggler).insertAfter(".sidebar-navigation li.submenu > a, .sidebar-navigation li.submenu > strong");
+    jQuery('.sidebar-navigation li.submenu.trail, .sidebar-navigation li.active').addClass('opened');
 
-        jQuery(this).closest('li').find('li.expanded').removeClass('expanded').children('ul').slideUp();
-        // Close opened sibling
-        jQuery(this).closest('li').siblings('li.expanded').removeClass('expanded').children('ul').slideUp();
-        // open/close item
-        jQuery(this).closest('li').toggleClass('expanded').children('ul').slideToggle();
-    });
+    setTimeout(function () {
+        jQuery('.sidebar-navigation .toggle-submenu').click(function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+
+            jQuery(this).closest('li').find('li.opened').removeClass('opened').children('ul').slideUp();
+            // Close opened sibling
+            jQuery(this).closest('li').siblings('li.opened').removeClass('opened').children('ul').slideUp();
+            // open/close item
+            jQuery(this).closest('li').toggleClass('opened').children('ul').slideToggle();
+        });
+    }, 20);
 });
